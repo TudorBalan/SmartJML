a0ab4f8528c6a49145d67502fed11f957679b507
TudorBalan <tudor.balan@icloud.com>
2025-05-21 13:45:40 +0200

    refactor Address type

--- src/main/java/types/Address.java
+++ src/main/java/types/Address.java
@@ -6,8 +6,6 @@ public class Address {
     //@ public invariant (\forall Address a; a.address >= 0);
     public final int address;
 
-    //@ public normal_behavior
-    //@ ensures this.address = address;
     public Address(int address) {
         this.address = address;
     }
